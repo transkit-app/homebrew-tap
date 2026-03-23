2b75b2dd401df137990854e7c63d9f6fecf47316
transkit-app <support@transkit.app>
2026-03-23 05:08:30 +0000

    transkit 3.1.5

--- Casks/transkit.rb
+++ Casks/transkit.rb
@@ -1,20 +1,20 @@
 cask "transkit" do
-    arch arm: "aarch64", intel: "x64"
+  arch arm: "aarch64", intel: "x64"
 
-    version "3.1.3"
-    sha256 arm:   "23cb363e80ff1d584f74aac8cb482d1145ee0525bcb762a02ee79fd6636307e2",
-           intel: "8a6a2cc42cb2576f37bbd40d7fb286d8e4ce84373d94199997d47ab4bcc0f2b4"
+  version "3.1.5"
+  sha256 arm:   "b35c3bc57662455177a7f575bc16611cc3d67e5a73d54966f0957d7c759dc1d1",
+         intel: "46279c4f2013e576aedfadc24e20570ab6ec6aedddcffddea3f326ee39b67a87"
 
-    url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_#{arch}.dmg"
-    name "TransKit"
-    desc "Cross-platform desktop app for translation, OCR, realtime monitor, and TTS"
-    homepage "https://github.com/transkit-app/transkit-desktop"
+  url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_#{arch}.dmg"
+  name "TransKit"
+  desc "Cross-platform desktop app for translation, OCR, realtime monitor, and TTS"
+  homepage "https://github.com/transkit-app/transkit-desktop"
 
-    app "TransKit.app"
-    binary "#{appdir}/TransKit.app/Contents/MacOS/TransKit"
+  app "TransKit.app"
+  binary "#{appdir}/TransKit.app/Contents/MacOS/TransKit"
 
-    zap trash: [
-      "~/Library/Application Support/com.transkit.desktop",
-      "~/Library/Caches/com.transkit.desktop",
-    ]
-  end
+  zap trash: [
+    "~/Library/Application Support/com.transkit.desktop",
+    "~/Library/Caches/com.transkit.desktop",
+  ]
+end
