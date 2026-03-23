ca49cd2c998c84d3b8904b18da24404db4981a46
Fern.N <vietlinhqv@gmail.com>
2026-03-23 12:58:04 +0700

    refactor: use `on_arm` and `on_intel` blocks for architecture-specific Cask definitions.

--- Casks/transkit.rb
+++ Casks/transkit.rb
@@ -1,11 +1,17 @@
 cask "transkit" do
-  arch arm: "aarch64", intel: "x64"
-
   version "3.1.3"
-  sha256 arm:   "23cb363e80ff1d584f74aac8cb482d1145ee0525bcb762a02ee79fd6636307e2",
-         intel: "8a6a2cc42cb2576f37bbd40d7fb286d8e4ce84373d94199997d47ab4bcc0f2b4"
 
-  url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_#{arch}.dmg"
+  on_arm do
+    sha256 "23cb363e80ff1d584f74aac8cb482d1145ee0525bcb762a02ee79fd6636307e2"
+
+    url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_aarch64.dmg"
+  end
+  on_intel do
+    sha256 "8a6a2cc42cb2576f37bbd40d7fb286d8e4ce84373d94199997d47ab4bcc0f2b4"
+
+    url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_x64.dmg"
+  end
+
   name "TransKit"
   desc "Cross-platform desktop app for translation, OCR, realtime monitor, and TTS"
   homepage "https://github.com/transkit-app/transkit-desktop"
