021fe389fc74f8ced8fcbf64a3bed68eb0597dc5
transkit-app <support@transkit.app>
2026-03-25 05:49:53 +0000

    transkit 3.2.0

--- Casks/transkit.rb
+++ Casks/transkit.rb
@@ -1,13 +1,13 @@
 cask "transkit" do
-  version "3.1.9"
+  version "3.2.0"
 
   on_arm do
-    sha256 "5821808c05e07f39efc69303445bd46ba22aab1669c0a86dd176be67fd31aadb"
+    sha256 "0797ee6e0d5c08bd76893cb1c0c5d4d0160bb496a2148e720a51b18468ba0908"
 
     url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "c51c29339d7a66f5e67c1d6befcc841a913dda41ed5ce722c50e75374f736d30"
+    sha256 "fd2aafbf5e21ed25fa851aa0d1a27b66d69b42ea8f353a08566d36e36060f706"
 
     url "https://github.com/transkit-app/transkit-desktop/releases/download/#{version}/TransKit_#{version}_x64.dmg"
   end
